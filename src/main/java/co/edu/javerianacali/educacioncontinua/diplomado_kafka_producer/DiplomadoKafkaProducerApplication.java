8cc08141184a91ff1a28f38d89e4552a25da8e91
samic05 <sakebi14@gmail.com>
2024-06-30 22:27:51 -0500

    first commit

---
+++ src/main/java/co/edu/javerianacali/educacioncontinua/diplomado_kafka_producer/DiplomadoKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javerianacali.educacioncontinua.diplomado_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomadoKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomadoKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javerianacali/educacioncontinua/diplomado_kafka_producer/DiplomadoKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javerianacali.educacioncontinua.diplomado_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomadoKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
